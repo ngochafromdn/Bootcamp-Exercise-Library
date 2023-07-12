4278322e44706c6274c1ec3c4fb5ba02e646f590
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-07-12 17:42:03 +0700

    Create test branch.

---
+++ test/test_myconverter.py
@@ -0,0 +1,28 @@
+import unittest
+from myconverter.converter import MyConverter
+import os
+
+
+class Test(unittest.TestCase):
+    def test_convert_png2png(self):
+        cvt = MyConverter(input_file_path="../test/testcases/test.png", output_file_path="out_test_png.png")
+        self.assertEqual(cvt.convert(), "Success. File is saved in out_test_png.png")
+    
+    def test_convert_heic2png(self):
+        cvt = MyConverter(input_file_path="../test/testcases/test.heic", output_file_path="out_test_heic.png")
+        self.assertEqual(cvt.convert(), "Success. File is saved in out_test_heic.png")
+    
+    def test_convert_tiff2png(self):
+        cvt = MyConverter(input_file_path="../test/testcases/test.tiff", output_file_path="out_test_tiff.png")
+        self.assertEqual(cvt.convert(), "Success. File is saved in out_test_tiff.png")
+    
+    def test_convert_pdf2png(self):
+        cvt = MyConverter(input_file_path="../test/testcases/test.pdf", output_file_path="out_test_pdf.png")
+        self.assertEqual(cvt.convert(), "Success. File is saved in out_test_pdf.png")
+
+if __name__ == "__main__":
+    # check if folder test_reults not exists, create it
+    if not os.path.exists("test_results"):
+        os.mkdir("test_results")
+    # run unittest
+    unittest.main()
