872f98ae55aca618e4141fae9d071a0075a6ea32
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-07-12 18:40:39 +0700

    Fix bugs unit tests.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Bootcamp-Exercise-Library
+
+### Create Anaconda environment
+
+```
+conda env create -f environment.yml
+conda activate cinna-lab2
+```
+
+### Run unit tests
+Please uninstall fitz before run the unit test
+
+```
+pip uninstall fitz
+```
+
+Run the following command:
+
+```
+python test/test_myconverter.py
+```
--- test/test_myconverter.py
+++ test/test_myconverter.py
@@ -2,27 +2,28 @@ import unittest
 from myconverter.converter import MyConverter
 import os
 
+TEST_RESULT_FOLDER = os.path.join("test", "test_results")
 
 class Test(unittest.TestCase):
     def test_convert_png2png(self):
-        cvt = MyConverter(input_file_path="/testcases/test.png", output_file_path="test_results/out_test_png.png")
-        self.assertEqual(cvt.convert(), "Success. File is saved in test_results/out_test_png.png")
+        cvt = MyConverter(input_file_path=os.path.join("test", "testcases", "test.png"), output_file_path=os.path.join("test", "test_results", "out_test_png.png"))
+        self.assertEqual(cvt.convert(), "Success. File is saved in test/test_results/out_test_png.png")
     
     def test_convert_heic2png(self):
-        cvt = MyConverter(input_file_path="/testcases/test.heic", output_file_path="test_results/out_test_heic.png")
-        self.assertEqual(cvt.convert(), "Success. File is saved in test_results/out_test_heic.png")
+        cvt = MyConverter(input_file_path=os.path.join("test", "testcases", "test.heic"), output_file_path=os.path.join("test", "test_results", "out_test_heic.png"))
+        self.assertEqual(cvt.convert(), "Success. File is saved in test/test_results/out_test_heic.png")
     
     def test_convert_tiff2png(self):
-        cvt = MyConverter(input_file_path="/testcases/test.tiff", output_file_path="test_results/out_test_tiff.png")
-        self.assertEqual(cvt.convert(), "Success. File is saved in test_results/out_test_tiff.png")
+        cvt = MyConverter(input_file_path=os.path.join("test", "testcases", "test.tiff"), output_file_path=os.path.join("test", "test_results", "out_test_tiff.png"))
+        self.assertEqual(cvt.convert(), "Success. File is saved in test/test_results/out_test_tiff.png")
     
     def test_convert_pdf2png(self):
-        cvt = MyConverter(input_file_path="/testcases/test.pdf", output_file_path="test_results/out_test_pdf.png")
-        self.assertEqual(cvt.convert(), "Success. File is saved in test_results/out_test_pdf.png")
+        cvt = MyConverter(input_file_path=os.path.join("test", "testcases", "test.pdf"), output_file_path=os.path.join("test", "test_results", "out_test_pdf.png"))
+        self.assertEqual(cvt.convert(), "Success. File is saved in test/test_results/out_test_pdf.png")
 
 if __name__ == "__main__":
     # check if folder test_reults not exists, create it
-    if not os.path.exists("test_results"):
-        os.mkdir("test_results")
+    if not os.path.exists(TEST_RESULT_FOLDER):
+        os.mkdir(TEST_RESULT_FOLDER)
     # run unittest
     unittest.main()
