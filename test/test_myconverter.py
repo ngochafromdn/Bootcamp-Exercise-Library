06e67cb6fd5b75b7d0571c992c682128c81676c5
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-07-12 17:57:54 +0700

    Complete unit tests.

--- test/test_myconverter.py
+++ test/test_myconverter.py
@@ -5,20 +5,20 @@ import os
 
 class Test(unittest.TestCase):
     def test_convert_png2png(self):
-        cvt = MyConverter(input_file_path="../test/testcases/test.png", output_file_path="out_test_png.png")
-        self.assertEqual(cvt.convert(), "Success. File is saved in out_test_png.png")
+        cvt = MyConverter(input_file_path="/testcases/test.png", output_file_path="test_results/out_test_png.png")
+        self.assertEqual(cvt.convert(), "Success. File is saved in test_results/out_test_png.png")
     
     def test_convert_heic2png(self):
-        cvt = MyConverter(input_file_path="../test/testcases/test.heic", output_file_path="out_test_heic.png")
-        self.assertEqual(cvt.convert(), "Success. File is saved in out_test_heic.png")
+        cvt = MyConverter(input_file_path="/testcases/test.heic", output_file_path="test_results/out_test_heic.png")
+        self.assertEqual(cvt.convert(), "Success. File is saved in test_results/out_test_heic.png")
     
     def test_convert_tiff2png(self):
-        cvt = MyConverter(input_file_path="../test/testcases/test.tiff", output_file_path="out_test_tiff.png")
-        self.assertEqual(cvt.convert(), "Success. File is saved in out_test_tiff.png")
+        cvt = MyConverter(input_file_path="/testcases/test.tiff", output_file_path="test_results/out_test_tiff.png")
+        self.assertEqual(cvt.convert(), "Success. File is saved in test_results/out_test_tiff.png")
     
     def test_convert_pdf2png(self):
-        cvt = MyConverter(input_file_path="../test/testcases/test.pdf", output_file_path="out_test_pdf.png")
-        self.assertEqual(cvt.convert(), "Success. File is saved in out_test_pdf.png")
+        cvt = MyConverter(input_file_path="/testcases/test.pdf", output_file_path="test_results/out_test_pdf.png")
+        self.assertEqual(cvt.convert(), "Success. File is saved in test_results/out_test_pdf.png")
 
 if __name__ == "__main__":
     # check if folder test_reults not exists, create it
