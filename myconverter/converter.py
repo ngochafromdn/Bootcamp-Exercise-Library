a9998a9e17b6b3fe1afe5c27ef8402093b15a383
ngochafromdn <nguyenhoangngocha.vn@gmail.com>
2023-07-13 12:04:58 +0700

    Fix comment/logging/import/requirement after review

--- myconverter/converter.py
+++ myconverter/converter.py
@@ -1,8 +1,9 @@
-from PIL import Image
+import os
+import shutil
 import pyheif
 import fitz
-import shutil
-import os
+from PIL import Image
+import logging
 
 
 class MyConverter:
@@ -15,16 +16,17 @@ class MyConverter:
         """
         self.input_file_path = input_file_path
         self.output_file_path = output_file_path
-        self.ext = self.file_extension()
+        self.ext = self.extension_file()
 
-    def file_extension(self) -> str:
+    def extension_file(self) -> str:
         """Get file extension"""
         return os.path.splitext(self.input_file_path)[1]
 
     def convert_png2png(self) -> str:
-        """Copy png file to output folder"""
+        """Convert png to png"""
         shutil.copyfile(self.input_file_path, self.output_file_path)
-        return f"Success. File is saved in {self.output_file_path}"
+        logging.info(f"Success. File is saved in {self.output_file_path}")
+        return True
 
     def convert_heic2png(self) -> str:
         """Convert heic to png"""
@@ -39,7 +41,8 @@ class MyConverter:
         )
         image.save(self.output_file_path)
 
-        return f"Success. File is saved in {self.output_file_path}"
+        logging.info(f"Success. File is saved in {self.output_file_path}")
+        return True
 
     def convert_tiff2png(self) -> str:
         """Convert tiff to png"""
@@ -47,7 +50,8 @@ class MyConverter:
         im.thumbnail(im.size)
         im.save(self.output_file_path, "png", quality=100)
 
-        return f"Success. File is saved in {self.output_file_path}"
+        logging.info(f"Success. File is saved in {self.output_file_path}")
+        return True
 
     def convert_pdf2png(self, dpi: int = 300) -> str:
         """Convert pdf to png"""
@@ -57,7 +61,8 @@ class MyConverter:
         for page in doc:
             pix = page.get_pixmap(matrix=magnify)
             pix.save(f"{self.output_file_path}-{page.number}.png")
-        return f"Success. File is saved in {self.output_file_path}"
+        logging.info(f"Success. File is saved in {self.output_file_path}")
+        return True
 
     def convert(self) -> str:
         """Main convert function"""
@@ -70,8 +75,8 @@ class MyConverter:
         elif self.ext == ".pdf":
             return self.convert_pdf2png()
         else:
-            return "Error. File extension is not supported."
-
+            logging.error("Error. File extension is not supported.")
+            return False
 
 # if __name__ == "__main__":
 #     cvt = MyConverter(input_file_path="../test/testcases/test.tiff", output_file_path="out_test.png")
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import setuptools
 
 setuptools.setup(
-    name="converter",
+    name="File-Converter-to-image",
     version="0.0.1",
+    description="A package for converting files to png",
+    author = "Fantastic Five",
     packages=setuptools.find_packages('.'),
 )
